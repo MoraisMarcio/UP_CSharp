edf84e342b7d6f573af650a1bd8f3c61b0877f3d
moraismarcio <marcioc.morais@hotmail.com>
2018-08-12 18:52:15 -0300

    atualizações eleiçao

--- Eleicoes/MainWindow.xaml.cs
+++ Eleicoes/MainWindow.xaml.cs
@@ -22,30 +22,65 @@ namespace Eleicoes
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();           
         }
 
         string nome;
         int num;
-        List<String> candidatosList = new List<string>();    
+        string nomeVoto;
+        int numVoto;
+        List<String> candidatosList = new List<string>();
+
+        private void campoNome_Initialized(object sender, EventArgs e)
+        {
+            this.campoNome.Focus();
+        }
 
         private void btnCadastrar_Click(object sender, RoutedEventArgs e)
         {
+            ListaCandidatos.Text = "";
             nome = Convert.ToString(campoNome.Text);
             num = Convert.ToInt32(campoNumero.Text);
 
             candidatosList.Add(nome);
             candidatosList.Add(Convert.ToString(num));
 
+            for (int i = 0; i < candidatosList.Count; i++)
+            {
+                ListaCandidatos.Text += candidatosList[i] + "\n";
+            }
+          
+            campoNome.Text = null;
+            campoNumero.Text = null;
        } 
 
         
-        private void Listar_Click(object sender, RoutedEventArgs e)
+        private void limparLista_Click(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i <= candidatosList.Count; i++)
+            ListaCandidatos.Text = "";
+
+        }       
+
+        private void btnVotar_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < candidatosList.Count; i++)
             {
-                ListaCandidatos.Text = candidatosList[i];
+                //if (candidatosList.Count < 1)
+                //{                   
+                    if (candidatosList[i] == campoNumeroVoto.Text)
+                    {
+                        campoNomeVoto.Text = candidatosList[i - 1];
+                    }
+                //}
+                //else
+                //{
+                //    textConfirmacaoVoto.Text = "ATENÇÃO: Não há candidatos cadastrados.";
+                //}
+                
+                 
             }
+            //get num e nome em Lista de candidatos cadastrados
+            //msg de erro caso não haja candidato cadastrado com numero solicitado
         }
     }
 }
