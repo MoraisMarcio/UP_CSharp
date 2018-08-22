f3b97219c33b11666204cb1ddd1f0241589dc46f
moraismarcio <marcioc.morais@hotmail.com>
2018-08-21 22:00:15 -0300

    Herdando DbContext do Entity e Criando Construtor para indicar o nome da String de Conexão

--- Aula2108_CodeFirst/Modelos/DAL/Contexto.cs
+++ Aula2108_CodeFirst/Modelos/DAL/Contexto.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Aula2108_CodeFirst.Modelos.DAL
 {
-    public class Contexto
+    public class Contexto : DbContext   //  using System.Data.Entity;
     {
+        //  Construtor
+        public Contexto() : base("nomeStringConexao")
+        {
 
+        }
     }
 }
