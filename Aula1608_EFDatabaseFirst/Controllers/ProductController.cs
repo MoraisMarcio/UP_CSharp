25f05e4fc6d03a823af680bc1d6daebe51002d5e
moraismarcio <marcioc.morais@hotmail.com>
2018-08-21 21:20:27 -0300

    Criando projeto Aula2108_CodeFirst

---
+++ Aula1608_EFDatabaseFirst/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1608_EFDatabaseFirst.Controllers
+{
+    class ProductController
+    {
+        void ProductInsert(Product product)
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            contexto.Product.Add(product);
+            contexto.SaveChanges();
+        }
+
+        List<Product> ProductList()
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            return contexto.Product.ToList();
+        }
+
+        Product ProductFindById(int id)
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            return contexto.Product.Find(id);         
+        }
+
+        void ProductRemove(int id)
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            Product productToRemove = contexto.Product.Find(id);
+
+            if (productToRemove != null)
+            {
+                contexto.Product.Remove(productToRemove);
+                contexto.SaveChanges();
+            }
+            
+        }
+
+        void ProductUpdate(int id, Product dadosAtualizados)
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            Product productToUpdate = contexto.Product.Find(id);
+
+            if (productToUpdate != null)
+            {
+                productToUpdate.Name = dadosAtualizados.Name;
+                productToUpdate.Color = dadosAtualizados.Color;
+
+                contexto.Entry(productToUpdate).State = System.Data.Entity.EntityState.Modified;
+                contexto.SaveChanges();
+            }          
+
+        }
+    }
+}
