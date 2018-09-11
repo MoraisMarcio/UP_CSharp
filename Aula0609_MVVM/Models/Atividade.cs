3460f0997e36c9fba5d227c820f420300d36977d
moraismarcio <marcioc.morais@hotmail.com>
2018-09-11 12:25:53 -0300

    Adicionando classe Atividade (Model)

---
+++ Aula0609_MVVM/Models/Atividade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula0609_MVVM.Models
+{
+    public class Atividade
+    {
+        public int AtividadeID { get; set; }
+        public string Titutlo { get; set; }
+        public string Descricao { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
